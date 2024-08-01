729d3197a67305c81ff0164c4afd4173e56d8285
Ptennisnet <148290329+Ptennisnet@users.noreply.github.com>
2024-08-01 14:35:05 +1000

    Save/load mazes
    Save maze to maze.txt file. Can import mazes from the savedCourses directory. To copy a course either change the name of the maze.txt file or copy the file itself, not the content (for some reason that screws everything up). Couple of sample mazes included.

--- mazeEditorTkinter.py
+++ mazeEditorTkinter.py
@@ -1,4 +1,7 @@
 import tkinter as tk
+from tkinter import *
+import re
+import os
 
 
 class MazeEditor(tk.Tk):
@@ -26,19 +29,34 @@ class MazeEditor(tk.Tk):
 
         # Buttons
         self.button_draw = tk.Button(self, text="Draw Obstacle", command=self.draw_mode)
-        self.button_draw.pack()
+        self.button_draw.pack(side="left")
 
         self.button_place_start = tk.Button(self, text="Place Start", command=self.place_start_mode)
-        self.button_place_start.pack()
+        self.button_place_start.pack(side="left")
 
         self.button_place_finish = tk.Button(self, text="Place Finish", command=self.place_finish_mode)
-        self.button_place_finish.pack()
+        self.button_place_finish.pack(side="left")
+
+        self.button_erase = tk.Button(self, text="Erase", command=self.erase_mode)
+        self.button_erase.pack(side="left")
 
         self.button_export = tk.Button(self, text="Export Maze", command=self.export_maze)
-        self.button_export.pack()
+        self.button_export.pack(side="right")
 
         self.button_clear = tk.Button(self, text="Clear Maze", command=self.clear_maze)
-        self.button_clear.pack()
+        self.button_clear.pack(side="right")
+
+        self.button_import = tk.Button(self, text="Import Matrix From File", command=self.import_maze)
+        self.button_import.pack(side="right")
+
+        # Drop-down menu
+        file_menu_options = os.listdir(os.getcwd() + "/savedCourses")
+
+        self.variable = StringVar()
+        self.variable.set(file_menu_options[0])
+        file_menu_drop = OptionMenu(self, self.variable, *file_menu_options)
+        file_menu_drop.pack(side="right")
+
 
     def create_matrix(self, matrix_width, matrix_height):
         print("create matrix")
@@ -50,17 +68,23 @@ class MazeEditor(tk.Tk):
         self.maze_matrix.insert(0, [1 for _ in range(matrix_width + 2)])
 
     def initialise_canvas(self):
-        print("initialise matrix")
+        print("initialise canvas")
         for y in range(len(self.maze_matrix)):
             for x in range(len(self.maze_matrix[0])):
+                square_dims = (x * self.cell_size,
+                               y * self.cell_size,
+                               (x + 1) * self.cell_size,
+                               (y + 1) * self.cell_size)
                 if self.maze_matrix[y][x] == 1:
-                    self.canvas.create_rectangle(
-                        x * self.cell_size,
-                        y * self.cell_size,
-                        (x + 1) * self.cell_size,
-                        (y + 1) * self.cell_size,
-                        fill="black"
-                    )
+                    self.canvas.create_rectangle(square_dims, fill="black")
+                if self.maze_matrix[y][x] == 2:
+                    self.canvas.create_rectangle(square_dims, fill="green")
+                    self.start_coords = [x, y]
+                    print(f"start coord: {x},{y}")
+                if self.maze_matrix[y][x] == 3:
+                    self.canvas.create_rectangle(square_dims, fill="red")
+                    self.finish_coords = [x, y]
+                    print(f"finish coord: {x},{y}")
 
     def draw_mode(self):
         print("draw mode")
@@ -77,10 +101,15 @@ class MazeEditor(tk.Tk):
         self.canvas.bind("<Button-1>", self.place_finish)
         self.canvas.unbind("<B1-Motion>")
 
+    def erase_mode(self):
+        print("erase mode")
+        self.canvas.bind("<B1-Motion>", self.erase)
+        self.canvas.bind("<Button-1>", self.erase)
+
     def draw(self, event):
         x = event.x // self.cell_size
         y = event.y // self.cell_size
-        if 0 <= x < self.grid_width and 0 <= y < self.grid_height:
+        if 0 <= x < self.grid_width + 2 and 0 <= y < self.grid_height + 2:
             self.maze_matrix[y][x] = 1
             self.canvas.create_rectangle(
                 x * self.cell_size,
@@ -94,8 +123,7 @@ class MazeEditor(tk.Tk):
         if (self.start_coords[0] < 0) and (self.start_coords[1] < 0):
             x = event.x // self.cell_size
             y = event.y // self.cell_size
-            if ((0 <= x < self.grid_width and 0 <= y < self.grid_height)
-                    and self.maze_matrix[y][x] == 0):
+            if 1 <= x < self.grid_width + 1 and 1 <= y < self.grid_height + 1:
                 self.maze_matrix[y][x] = 2
                 self.start_coords = [x, y]
                 self.canvas.create_rectangle(
@@ -111,8 +139,7 @@ class MazeEditor(tk.Tk):
         if (self.finish_coords[0] < 0) and (self.finish_coords[1] < 0):
             x = event.x // self.cell_size
             y = event.y // self.cell_size
-            if ((0 <= x < self.grid_width and 0 <= y < self.grid_height)
-                    and self.maze_matrix[y][x] == 0):
+            if 1 <= x < self.grid_width + 1 and 1 <= y < self.grid_height + 1:
                 self.finish_coords = [x, y]
                 self.maze_matrix[y][x] = 3
                 self.canvas.create_rectangle(
@@ -124,9 +151,29 @@ class MazeEditor(tk.Tk):
                 )
                 print(self.finish_coords)
 
+    def erase(self, event):
+        x = event.x // self.cell_size
+        y = event.y // self.cell_size
+        if 1 <= x < self.grid_width + 1 and 1 <= y < self.grid_height + 1:
+            # Reset start/finish coords
+            if self.maze_matrix[y][x] == 2:
+                self.start_coords = [-1, -1]
+            if self.maze_matrix[y][x] == 3:
+                self.finish_coords = [-1, -1]
+            self.maze_matrix[y][x] = 0
+            self.canvas.create_rectangle(
+                x * self.cell_size,
+                y * self.cell_size,
+                (x + 1) * self.cell_size,
+                (y + 1) * self.cell_size,
+                fill="white", outline="white"
+            )
+
     def export_maze(self):
-        for row in self.maze_matrix:
-            print(row)
+        with open("savedCourses/maze.txt", "w") as maze_file:
+            for row in self.maze_matrix:
+                maze_file.write(str(row) + "\n")
+                print(row)
 
     def clear_maze(self):
         print("cleared")
@@ -135,6 +182,29 @@ class MazeEditor(tk.Tk):
         self.initialise_canvas()
         self.start_coords = [-1, -1]
         self.finish_coords = [-1, -1]
+
+    def import_maze(self):
+        import_file_path = "savedCourses/" + self.variable.get()
+        print(f"import from text file: {import_file_path}")
+        self.maze_matrix = []
+        with open(import_file_path, 'r') as maze_file:
+            for line in maze_file:
+                # for sub in re.split(",", line[1:-2]):
+                #     print(int(sub.strip()))
+                integer_list = [int(sub.strip()) for sub in re.split(",", line[1:-2])]
+                self.maze_matrix.append(integer_list)
+        self.canvas.delete("all")
+        self.initialise_canvas()
+
+    def update_path(self, x, y):
+        print(f"Update cell ({x},{y})")
+        self.canvas.create_rectangle(
+            x * self.cell_size,
+            y * self.cell_size,
+            (x + 1) * self.cell_size,
+            (y + 1) * self.cell_size,
+            fill="grey", outline="grey"
+        )
 
 
 if __name__ == "__main__":
