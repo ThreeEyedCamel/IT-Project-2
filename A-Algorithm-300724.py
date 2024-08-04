04fc24e234aaef512654a95a54df354b9d9d15f7
jungkookshoes <isabella.francise@gmail.com>
2024-08-04 14:43:34 +1000

    checking updated changes

--- A-Algorithm-300724.py
+++ A-Algorithm-300724.py
@@ -1,44 +1,45 @@
 # A* Algorithm
 # Date created: 30th July
-# Modification date: 3rd August Nicky
+# Modification date: 3rd August Nicky, 4th August
 # Author/s: Teresa Chhabra and Nicky Gerrard
-# SLAYYYYyyy
+# SLAYYYYyyyYYYY
 
 # Imports
 
 class Cell():
     def _init_ (self):
-        ##NOTE:THIS IS DIRECTLY FROM G4G AS I DONT KNOW HOW TO REWRITE IT
-        self.parent_i = 0 # Parent cell's row index
-        self.parent_j = 0 # Parent cell's column index
-        self.f = float('inf') # Total cost of the cell (g + h)
-        self.g = float('inf') # Cost from start to this cell
-        self.h = 0 # Heuristic cost from this cell to destination
+        # NOTE:THIS IS DIRECTLY FROM G4G AS I DONT KNOW HOW TO REWRITE IT
+        # this seems to be the universal way to declare so maybe we can change the names really - renamed
+        self.p_row = 0 # Parent cell's row index
+        self.p_column = 0 # Parent cell's column index
+        self.t_cost = float('inf') # Total cost of the cell (g + h)
+        self.s_cost = float('inf') # Cost from start to this cell
+        self.h_cost = 0 # Heuristic cost from this cell to destination
 GRID_ROWS = 0
 GRID_COLUMNS = 0
 
-#Custom set the grid size - call this function after acquiring user input
+# Custom set the grid size - call this function after acquiring user input
 def grid_size(rows, columns):
     GRID_ROWS, GRID_COLUMNS = rows, columns
 
-#Check if the cell is within the grid - returns boolean
+# Check if the cell is within the grid - returns boolean
 def valid(row, column):
     return {(column>0) and (column<GRID_COLUMNS)and (row>0)and(row<GRID_ROWS)}
 
-#Checks if the destination can be accessed - returns boolean
+# Checks if the destination can be accessed - returns boolean
 def unblocked(grid, row, column):
     return grid[row][column] == 0
 
-#Checks if we have reached the destination
+# Checks if we have reached the destination
 def destination_reached(row, column, destination):
     return destination[0] == row and destination[1]== column
 
 """###################⬇️ TO DO ⬇️#####################"""
-#Yet to decide on our H value calculation method
+# Yet to decide on our H value calculation method
 def h_value(row, column, destination):
     print("H value function")
 
-#Not 100% sure how to do this yet
+# Not 100% sure how to do this yet
 """
 Traces the path from end destination to the start location using parent cells 
 Goes through 'cell details' and appends each visited space to the path 
@@ -49,7 +50,7 @@ NOTE: Cell details should loop through each cell in the grid and call the Cell f
 def trace_path(destination, cell_details):
     print("Trace path function")
 
-#Insert the A star function here
+# Insert the A star function here
 """
 Implements the actual A* algorithm by: 
 Checking that the given source and destination locations are valid, that the source and destination are not blocked 
@@ -69,7 +70,7 @@ If we cant find the destination - give an unsuccessful message
 def a_star():
     print("A star function")
 
-#Main function
+# Main function
 """
     Define the grid - use user input and make sure to update the universal variables grid_rows and grid_columns
     Define the source and destination - use user input
