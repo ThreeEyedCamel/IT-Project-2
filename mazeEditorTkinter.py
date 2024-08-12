235d861cac913c99999c7aecfad853a1a85c4a91
BigCheezel <148037932+BigCheezel@users.noreply.github.com>
2024-08-12 10:39:57 +1000

    UI button overhaul

--- mazeEditorTkinter.py
+++ mazeEditorTkinter.py
@@ -2,6 +2,7 @@ import tkinter as tk
 from tkinter import *
 import re
 import os
+from PIL import Image, ImageTk
 
 
 class MazeEditor(tk.Tk):
@@ -28,39 +29,77 @@ class MazeEditor(tk.Tk):
         self.start_coords = [-1, -1]
         self.finish_coords = [-1, -1]
 
-        # Buttons
-        self.button_draw = tk.Button(self, text="Draw Obstacle", command=self.draw_mode)
-        self.button_draw.pack(side="left")
+        # Images
+        self.brush_image = Image.open(r"images/paintbrush.png").resize((32, 32))
+        self.brush_image = ImageTk.PhotoImage(self.brush_image)
 
-        self.button_place_start = tk.Button(self, text="Place Start", command=self.place_start_mode)
-        self.button_place_start.pack(side="left")
+        self.export_image = Image.open(r"images/export.png").resize((32, 32))
+        self.export_image = ImageTk.PhotoImage(self.export_image)
 
-        self.button_place_finish = tk.Button(self, text="Place Finish", command=self.place_finish_mode)
-        self.button_place_finish.pack(side="left")
+        self.clear_image = Image.open(r"images/trash.png").resize((32, 32))
+        self.clear_image = ImageTk.PhotoImage(self.clear_image)
 
-        self.button_erase = tk.Button(self, text="Erase", command=self.erase_mode)
-        self.button_erase.pack(side="left")
+        self.eraser_image = Image.open(r"images/eraser.png").resize((32, 32))
+        self.eraser_image = ImageTk.PhotoImage(self.eraser_image)
 
-        self.button_export = tk.Button(self, text="Export Maze", command=self.export_maze)
-        self.button_export.pack(side="right")
+        self.finish_image = Image.open(r"images/finish.png").resize((32, 32))
+        self.finish_image = ImageTk.PhotoImage(self.finish_image)
 
-        self.button_clear = tk.Button(self, text="Clear Maze", command=self.clear_maze)
-        self.button_clear.pack(side="right")
+        self.start_image = Image.open(r"images/start.png").resize((32, 32))
+        self.start_image = ImageTk.PhotoImage(self.start_image)
 
-        self.button_import = tk.Button(self, text="Import Matrix From File", command=self.import_maze)
-        self.button_import.pack(side="right")
+        self.import_image = Image.open(r"images/import.png").resize((32, 32))
+        self.import_image = ImageTk.PhotoImage(self.import_image)
 
-        # Test button
-        self.button_test_animation = tk.Button(self, text="test animation", command=self.animate_path_test)
-        self.button_test_animation.pack(side="left")
+        # Create a frame at the bottom of the window for buttons and labels
+        self.button_frame = tk.Frame(self)
+        self.button_frame.pack(side="bottom", fill="x")
+
+        # Buttons and labels in a grid layout
+        self.button_draw = tk.Button(self.button_frame, image=self.brush_image, command=self.draw_mode)
+        self.button_draw.grid(row=0, column=0, padx=5, pady=5)
+        self.button_label_draw = tk.Label(self.button_frame, text="Draw Obstacle")
+        self.button_label_draw.grid(row=1, column=0)
+
+        self.button_place_start = tk.Button(self.button_frame, image=self.start_image, command=self.place_start_mode)
+        self.button_place_start.grid(row=0, column=1, padx=5, pady=5)
+        self.button_label_start = tk.Label(self.button_frame, text="Place Start")
+        self.button_label_start.grid(row=1, column=1)
+
+        self.button_place_finish = tk.Button(self.button_frame, image=self.finish_image, command=self.place_finish_mode)
+        self.button_place_finish.grid(row=0, column=2, padx=5, pady=5)
+        self.button_label_finish = tk.Label(self.button_frame, text="Place Finish")
+        self.button_label_finish.grid(row=1, column=2)
+
+        self.button_erase = tk.Button(self.button_frame, image=self.eraser_image, command=self.erase_mode)
+        self.button_erase.grid(row=0, column=3, padx=5, pady=5)
+        self.button_label_erase = tk.Label(self.button_frame, text="Erase")
+        self.button_label_erase.grid(row=1, column=3)
+
+        self.button_export = tk.Button(self.button_frame, image=self.export_image, command=self.export_maze)
+        self.button_export.grid(row=0, column=4, padx=5, pady=5)
+        self.export_label = tk.Label(self.button_frame, text="Export Maze")
+        self.export_label.grid(row=1, column=4)
+
+        self.button_clear = tk.Button(self.button_frame, image=self.clear_image, command=self.clear_maze)
+        self.button_clear.grid(row=0, column=5, padx=5, pady=5)
+        self.clear_label = tk.Label(self.button_frame, text="Clear Maze")
+        self.clear_label.grid(row=1, column=5)
+
+        self.button_import = tk.Button(self.button_frame, image=self.import_image, command=self.import_maze)
+        self.button_import.grid(row=0, column=6, padx=5, pady=5)
+        self.import_label = tk.Label(self.button_frame, text="Import Matrix")
+        self.import_label.grid(row=1, column=6)
+
+        self.button_test_animation = tk.Button(self.button_frame, text="Test Animation", command=self.animate_path_test)
+        self.button_test_animation.grid(row=0, column=7, padx=5, pady=5)
 
         # Drop-down menu
         file_menu_options = os.listdir(os.getcwd() + "/savedCourses")
-
         self.variable = StringVar()
         self.variable.set(file_menu_options[0])
-        file_menu_drop = OptionMenu(self, self.variable, *file_menu_options)
-        file_menu_drop.pack(side="right")
+        file_menu_drop = OptionMenu(self.button_frame, self.variable, *file_menu_options)
+        file_menu_drop.grid(row=0, column=8, padx=5, pady=5)
 
     def create_matrix(self, matrix_width, matrix_height):
         print("create matrix")
